4b91f6530c8f00c78531601d4825fd958ebc42a8
Mark J. Handy <mjhandy@outlook.com>
2024-08-25 09:53:52 -0300

    mobile button updates

--- web-prototype/src/app/components/header/aux-nav/aux-nav.component.ts
+++ web-prototype/src/app/components/header/aux-nav/aux-nav.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 
+import {MatRippleModule} from '@angular/material/core';
 import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'header-aux-nav',
   standalone: true,
-  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+  imports: [MatIconModule, MatRippleModule],
   templateUrl: './aux-nav.component.html',
   styleUrl: './aux-nav.component.scss'
 })
 export class AuxNavComponent {
 
   mobileToggleActive = [false, false];
+
+  radius : number | undefined;
+  color: string | undefined;
 
   toggleClass(id: number) {
     this.mobileToggleActive[id] = !this.mobileToggleActive[id];
