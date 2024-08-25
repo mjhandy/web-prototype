b9dc097e21cd8ffa2e6e03f6835a580453de3e31
Mark J. Handy <mjhandy@outlook.com>
2024-08-25 09:10:04 -0300

    add Material design
    Set up basic toggle button for mobile nav

--- web-prototype/src/app/components/header/aux-nav/aux-nav.component.ts
+++ web-prototype/src/app/components/header/aux-nav/aux-nav.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
 @Component({
   selector: 'header-aux-nav',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './aux-nav.component.html',
   styleUrl: './aux-nav.component.scss'
 })
