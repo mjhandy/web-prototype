9c818016d6a831314401a3d96f46696cdcbc25c6
Mark J. Handy <mjhandy@outlook.com>
2024-11-11 11:08:59 -0400

    validation patterns

--- web-prototype/src/app/pages/form/services/question.service.ts
+++ web-prototype/src/app/pages/form/services/question.service.ts
@@ -21,7 +21,7 @@ export class QuestionService {
         label: 'forms.label.fName',
         value: '',
         required: true,
-        vpattern: "^[a-zA-Z' ]+$",
+        vpattern: "^[a-zA-Z']+$",
         order: 1,
       
       }),
@@ -30,7 +30,7 @@ export class QuestionService {
         label: 'forms.label.lName',
         value: '',
         required: true,
-        vpattern: "^[a-zA-Z' ]+$",
+        vpattern: "^[a-zA-Z']+$",
         order: 2,
       }),      
       new TextboxQuestion({
@@ -41,6 +41,13 @@ export class QuestionService {
         vpattern: "^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
         order: 3,
       }),
+      new TextboxQuestion({
+        key: 'address',
+        label: 'forms.label.address',
+        type: 'text',
+        required: true,
+        order: 4,
+      }), 
       new DropdownQuestion({
         key: 'country',
         label: 'forms.label.country',
@@ -50,7 +57,7 @@ export class QuestionService {
           {key: 'uk', value: 'United Kingdom of Great Britian'},
           {key: 'fr', value: 'France'},
         ],
-        order: 4,
+        order: 5,
         class: 'col-6'
       }),
       new DropdownQuestion({
@@ -60,9 +67,27 @@ export class QuestionService {
           {key: 'ny', value: 'New Yorl'},
           {key: 'fl', value:'Florida'}
         ],
-        order: 4,
+        order: 6,
         class: 'col-6'
-      })
+      }),
+      new TextboxQuestion({
+        key: 'zipCode',
+        label: 'forms.label.usZipCode',
+        type: 'text',
+        required: true,
+        vpattern: "^[0-9]{5}(?:-[0-9]{4})?$",
+        order: 7,
+        class: 'w-50'
+      }), 
+      new TextboxQuestion({
+        key: 'postalCode',
+        label: 'forms.label.canPostalCode',
+        type: 'text',
+        required: true,
+        vpattern: "^(?!.*[DFIOQU])[A-VXY][0-9][A-Z] ?[0-9][A-Z][0-9]$",
+        order: 8,
+        class: 'w-50'
+      }),
     ];
     return of(questions.sort((a, b) => a.order - b.order));
   }
