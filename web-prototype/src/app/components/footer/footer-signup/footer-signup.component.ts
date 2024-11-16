28bece6dcdbd243753ae8712f29fd19219c8250f
Mark J. Handy <mjhandy@outlook.com>
2024-11-16 10:39:51 -0400

    footer - sign up
    Redirect to sign up page

--- web-prototype/src/app/components/footer/footer-signup/footer-signup.component.ts
+++ web-prototype/src/app/components/footer/footer-signup/footer-signup.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, input, ViewChild } from '@angular/core';
+import { LocalStorageService } from '../../../services/local-storage-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'footer-signup',
@@ -8,5 +10,32 @@ import { Component } from '@angular/core';
   styleUrl: './footer-signup.component.scss'
 })
 export class FooterSignupComponent {
+  @ViewChild('input') footerEmail: any;
+
+  inputValue: string  = '';
+  footerSignUpForm: string = '';
+  
+  constructor(
+    private LocalStorageService: LocalStorageService,
+    private router: Router,
+  ) {}
+
+  submit(value: string){
+
+    // the input value
+    this.inputValue = value;
+    // set the field value in to local storage
+    this.LocalStorageService.setItem('footerEmail', value);
+    // clear input field before redirecting    
+
+    console.log('clicked', this.inputValue);      
+
+    // redirect to the sign up page if the field has a value
+    if (this.inputValue != ''){
+      console.log('input cleared', this.inputValue);
+      this.footerEmail.value = '';
+      this.router.navigate(['en/sign-up-dynamic']);
+    }
+  }
 
 }
