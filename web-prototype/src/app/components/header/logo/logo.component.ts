c58fb6f28f44cf27ed3f7f4546a8c442b5157eb1
Mark J. Handy <mjhandy@outlook.com>
2025-01-02 09:21:40 -0400

    Logo updates
    * fix router issues
    * fix dark mode issue

--- web-prototype/src/app/components/header/logo/logo.component.ts
+++ web-prototype/src/app/components/header/logo/logo.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'header-logo',
   standalone: true,
-  imports: [ MatIconModule ],
+  imports: [ MatIconModule, RouterModule ],
   templateUrl: './logo.component.html',
   styleUrl: './logo.component.scss'
 })
