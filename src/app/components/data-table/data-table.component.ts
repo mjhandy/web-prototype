f512625a3531cd59305d44b41d93b1870fe5e8fd
Mark J. Handy <mjhandy@outlook.com>
2025-01-07 11:30:58 -0400

    Update data-table.component.ts

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -1,17 +1,31 @@
 import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+
+export interface SalesInfo{
+  name: string,
+  sales: number,
+  department: string
+}
+
+const sales_data = SalesInfo[] = [
+  { name: 'Bob', sales: 2499.00, department: 'Furniture' },
+  { name: 'Jim', sales: 10000.00, department: 'Automotive' }
+] 
 
 @Component({
   selector: 'aap-data-table',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './data-table.component.html',
   styleUrl: './data-table.component.scss'
 })
 export class DataTableComponent {
 
-  td = [
-    { name: 'Bob', sales: '$2499.00', department: 'Furniture' },
-    { name: 'Jim', sales: '$10,000.00', department: 'Automotive' }
+  displayColumns: string[] = [
+    'col-name',
+    'col-sales',
+    'col-department'
   ]
+  dataSource = sales_data;
 
 }
