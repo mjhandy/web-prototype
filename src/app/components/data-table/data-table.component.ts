6f6ab8cb6b4e5dacbfaf58825c9e11bfb7186bf3
Mark J. Handy <mjhandy@outlook.com>
2025-01-08 17:54:01 -0400

    table sorting

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { AfterViewInit, Component, inject, ViewChild } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
 
 export interface SalesInfo {
   id: number,
@@ -7,28 +9,48 @@ export interface SalesInfo {
   sales: number,
   department: string
 }
-
 const SALES_DATA: SalesInfo[] = [
-  { id:1, name: 'Bob', sales: 1234.45, department: 'Appliances'},
-  { id:2, name: 'Alice', sales: 87654.23, department: 'Electronics'}
+  { id: 1, name: 'Bob', sales: 1234.45, department: 'Appliances' },
+  { id: 2, name: 'Alice', sales: 87654.23, department: 'Electronics' },
+  { id: 3, name: 'John', sales: 683.91, department: 'Hardware' },
+  { id: 4, name: 'Frank', sales: 256.87, department: 'Automotive' }
 ];
-
-
 
 @Component({
   selector: 'aap-data-table',
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatSortModule],
   standalone: true,
   templateUrl: './data-table.component.html',
   styleUrl: './data-table.component.scss'
 })
-export class DataTableComponent {
+export class DataTableComponent implements AfterViewInit {
+  private _liveAnnouncer = inject(LiveAnnouncer);
 
   displayColumns: string[] = [
     'col-name',
     'col-sales',
     'col-department'
   ]
-  dataSource = SALES_DATA;
+  dataSource = new MatTableDataSource(SALES_DATA);
+
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 
 }
