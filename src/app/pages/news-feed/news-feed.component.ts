e566f5f4b99f8c0323d86f6be9ce3690c13d386c
Mark J. Handy <mjhandy@outlook.com>
2025-02-02 18:19:57 -0400

    loading spinner

--- src/app/pages/news-feed/news-feed.component.ts
+++ src/app/pages/news-feed/news-feed.component.ts
@@ -1,30 +1,42 @@
 import { Component } from '@angular/core';
 import { NewsFeedService } from '../../services/news-feed.service';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
     selector: 'app-news-feed',
-    imports: [],
+    imports: [MatProgressSpinnerModule],
     templateUrl: './news-feed.component.html',
     styleUrl: './news-feed.component.scss'
 })
 export class NewsFeedComponent {
 
   story : any;
+  isLoading = true;
 
   constructor(private nsewsFeedService: NewsFeedService) { }
 
 
   ngOnInit() {
 
+    // this.nsewsFeedService.getNews()
+    //   .subscribe(response => {
+        
+    //     this.story = response;
+    //     this.story = this.story.articles;
+        
+    //     console.log('Article', this.story);
+    //     console.log('Article Title:', this.story[0].title)
+    //   });
+
     this.nsewsFeedService.getNews()
-      .subscribe(response => {
-        
-        this.story = response;
-        this.story = this.story.articles;
-        
-        console.log('Article', this.story);
-        console.log('Article Title:', this.story[0].title)
-      });
+      .subscribe(
+        data => {
+          this.story = data;
+          this.story = this.story.articles;
+          this.isLoading = false;
+        }
+      );
+
   }
 
 }
