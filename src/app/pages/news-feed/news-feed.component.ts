83d497145c4e6e82958e9e3d4f2b1fac4b513b86
Mark J. Handy <mjhandy@outlook.com>
2025-02-03 10:27:59 -0400

    error handeling

--- src/app/pages/news-feed/news-feed.component.ts
+++ src/app/pages/news-feed/news-feed.component.ts
@@ -1,41 +1,53 @@
 import { Component } from '@angular/core';
 import { NewsFeedService } from '../../services/news-feed.service';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { catchError, throwError } from 'rxjs';
+
+
 
 @Component({
-    selector: 'app-news-feed',
-    imports: [MatProgressSpinnerModule],
-    templateUrl: './news-feed.component.html',
-    styleUrl: './news-feed.component.scss'
+  selector: 'app-news-feed',
+  imports: [MatProgressSpinnerModule],
+  templateUrl: './news-feed.component.html',
+  styleUrl: './news-feed.component.scss'
 })
 export class NewsFeedComponent {
 
-  story : any;
+  story: any;
   isLoading = true;
+  isError = false;
+  errorMessage : string | undefined;
 
   constructor(private nsewsFeedService: NewsFeedService) { }
 
 
   ngOnInit() {
 
-    // this.nsewsFeedService.getNews()
-    //   .subscribe(response => {
-        
-    //     this.story = response;
-    //     this.story = this.story.articles;
-        
-    //     console.log('Article', this.story);
-    //     console.log('Article Title:', this.story[0].title)
-    //   });
+    this.nsewsFeedService.getNews().subscribe({
+      next: (data) => {
+        this.story = data;
+        this.story = this.story.articles;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.isError = true;
+        this.errorMessage = error.error.message;
+       console.log('error', error)
+      }
+    })
 
-    this.nsewsFeedService.getNews()
-      .subscribe(
-        data => {
-          this.story = data;
-          this.story = this.story.articles;
-          this.isLoading = false;
-        }
-      );
+
+    // this.nsewsFeedService.getNews()
+    //   .subscribe(
+    //     data => {
+    //       this.story = data;
+    //       this.story = this.story.articles;
+    //       this.isLoading = false;
+    //     },
+    //     // error => {
+    //     //   console.log ('loading error');
+    //     // }
+    //   );
 
   }
 
